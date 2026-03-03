1e2c5feda0155c83dc165e5d5fdb4cd1413967fb
Nathan <nathan.singh@inreachsw.com>
2026-03-03 15:22:42 +1100

    dashboard wip

---
+++ AnotherToolBox/Views/Controls/Player/StoryChecks.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AnotherToolBox.Views.Controls.Player;
+
+public partial class StoryChecks : UserControl
+{
+    public StoryChecks()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AnotherToolBox/Views/Controls/Player/Timers.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AnotherToolBox.Views.Controls.Player;
+
+public partial class Timers : UserControl
+{
+    public Timers()
+    {
+        InitializeComponent();
+    }
+}
