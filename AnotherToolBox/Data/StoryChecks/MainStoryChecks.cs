dfbb8b182c680b8e208f1b71a532656d857d426e
Hovrawl <nathan.singh@hotmail.com>
2026-03-08 10:06:59 +1100

    start smart enum for story check wip

---
+++ AnotherToolBox/Data/StoryChecks/MainStoryChecks.cs
@@ -0,0 +1,14 @@
+﻿using AnotherToolBox.Models.StoryChecks;
+using Ardalis.SmartEnum;
+
+namespace AnotherToolBox.Data.StoryChecks;
+
+public partial class StoryCheckEnum
+{
+
+    public static readonly StoryCheckEnum MainStoryPartOne = 
+        new(nameof(MainStoryPartOne), 1, StoryType.MainStory, "Main Story 1")
+    {
+        SubTitle = "Another Eden",
+    };
+}
---
+++ AnotherToolBox/Data/StoryChecks/StoryCheckEnum.cs
@@ -0,0 +1,23 @@
+﻿using AnotherToolBox.Models.StoryChecks;
+using Ardalis.SmartEnum;
+
+namespace AnotherToolBox.Data.StoryChecks;
+
+public partial class StoryCheckEnum : SmartEnum<StoryCheckEnum>
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? SubTitle { get; set; }
+    public string? Description { get; set; }
+    public string? Image { get; set; }
+    public StoryType Type { get; set; }
+    
+    public StoryCheckEnum(string name,
+        int value,
+        StoryType storyType,
+        string title) : base(name, value)
+    {
+        Type = storyType;
+        Title = title;
+    }
+}
